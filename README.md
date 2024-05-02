e98709034041cbebb4a9fcda3c8bbc4f4fa2e0b8
zinzin <zinzin@zinzins-MBP.lan>
2024-05-02 07:53:55 -0700

    up

---
+++ README.md
@@ -0,0 +1,1 @@
+A casual game developed using Flutter. 
---
+++ android/app/src/main/kotlin/com/example/game_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.karscodes.cat_smth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
